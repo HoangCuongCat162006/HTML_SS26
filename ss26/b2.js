ad44ff653f47c91701d3994c2f384477c8ebd79b
HoangCuongCat162006 <Cathoang162006@gmail.com>
2025-03-13 00:29:40 +0700

    Update b2.js

--- ss26/b2.js
+++ ss26/b2.js
@@ -1,8 +1,16 @@
-let str=prompt("Nhap vào chuỗi có độ dài lớn hơn 5 kí tự");
-let arr=str.split(' ');
-if(arr.length===0){
-    console.log("Mang khong co phan tu");
-} else{
+function chain(arr){
+    if(!Array.isArray(arr)){
+        console.log("Du lieu khong hop le");
+        return[];
+    }
+    if(arr.length===0){
+        console.log("Khong co du lieu nao");
+    }
+
     let result=arr.filter((Element) => Element.length>5);
-    console.log(result);
+    return result;
+
 }
+console.log(chain(["apple", "banana", "cat", "elephant", "dog"]));
+console.log(chain([]));
+console.log(chain("abc"));
